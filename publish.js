fcc6c1866966b67d6e6d03d508619e97ee3ac0d5
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:02:09 -0500

    New cl-publish system.

---
+++ publish.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+	dist: path.resolve(__dirname, 'dist'),
+	files: [
+		'^Course\\.js', '^Calendar\\.js', '^CourseConsole\\.js'
+	]
+}
--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,9 @@
 const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
 	entry: {
 		Course: path.resolve(__dirname, './index.js'),
 		Calendar: path.resolve(__dirname, './js/Calendar/Calendar.js'),
 		CourseConsole: path.resolve(__dirname, './js/Console/index.js')
-	},
-	plugins: [
-		new FileManagerPlugin({
-			onEnd: {
-				copy: [
-					{source: path.resolve(__dirname, '../../../cl/dist/course.*'), destination: path.resolve(__dirname, 'dist') },
-					{source: path.resolve(__dirname, '../../../cl/dist/calendar.*'), destination: path.resolve(__dirname, 'dist') },
-					{source: path.resolve(__dirname, '../../../cl/dist/courseconsole.*'), destination: path.resolve(__dirname, 'dist') }
-				]
-			}
-		})
-	]
+	}
 }
