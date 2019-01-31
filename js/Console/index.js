3d1258fb2e303e78bae13b1e64e89abdabaed581
cbowen <cbowen@cse.msu.edu>
2019-01-31 13:40:40 -0500

    Added some exports. Fresh Build.

--- js/Console/index.js
+++ js/Console/index.js
@@ -5,11 +5,13 @@ import PrevNextMemberVue from './Members/PrevNextMember.vue';
 import MemberFetcherComponentVue from './Members/MemberFetcherComponent.vue';
 import SubmissionsAssignmentMemberComponentVue from './SubmissionsAssignmentMemberComponent.vue';
 import MembersFetcherComponentVue from './Members/MembersFetcherComponent.vue';
+import SubmissionsLinksVue from './SubmissionsLinks.vue';
 
 Site.PrevNextMemberVue = PrevNextMemberVue;
 Site.MemberFetcherComponentVue = MemberFetcherComponentVue;
 Site.SubmissionsAssignmentMemberComponentVue = SubmissionsAssignmentMemberComponentVue;
 Site.MembersFetcherComponentVue = MembersFetcherComponentVue;
+Site.SubmissionsLinksVue = SubmissionsLinksVue;
 
 let courseConsole = new CourseConsole(Site.site);
 Site.site.console.course = courseConsole;
